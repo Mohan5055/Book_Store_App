d973bdf2e704477f727767806eade5ac3d516a62
Anuj Patel <anujpatel5055@gmail.com>
2023-09-10 13:47:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+const app = express();
+
+//config the dotenv file
+dotenv.config(); 
+
+// Middleware for parsing request body
+app.use(express.json());
+
+
+// Middleware for handling CORS POLICY
+app.use(cors());
+
+const PORT=process.env.PORT || 5555
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+  .connect(process.env.mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
